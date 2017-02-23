3eec486abaceefb04bc0f9834d41b0c9e7b14d29
mtnview101 <mtnview101@gmail.com>
2017-02-23 14:05:15 -0800

    commit 3

--- src/core/App.java
+++ src/core/App.java
@@ -10,5 +10,5 @@ public class App {
        @Path("/dmitry")
        public String qa() {
     	   //Execute your web service in your browser: (http://localhost:89/RESTfull_3_WS/microsoft/dmitry)
-             return "This is Microsoft web service using GET method by Dmitry Nakhabtsev"; }}
+             return "Today is Postman event day. Web service is using GET method by Dmitry Nakhabtsev"; }}
 
